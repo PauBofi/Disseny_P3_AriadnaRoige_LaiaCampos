6fc8dc52ebafd59ebf2cc5ae2c677987003e9726
PauBofi <131433590+PauBofi@users.noreply.github.com>
2025-05-14 17:27:37 +0200

    Incomplete enemy IA

--- Assets/Phantom.cs
+++ Assets/Phantom.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Phantom : MonoBehaviour
 {
     public Transform player;
     public float chaseSpeed = 2f;
-    public float jumpForce = 2f;
+    public float jumpForce = 10f;
+    public float maxReachableDistance = 5f;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
     private bool isGrounded;
-    private bool shouldJump; 
+    private bool shouldJump;
+    private float jumpForceToApply;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,28 +25,49 @@ public class Phantom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+        Debug.Log("Grounded: " + isGrounded + ", Velocity: " + rb.velocity.x);
 
-        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, 1 << player.gameObject.layer);
+        isGrounded = rb.IsTouchingLayers(groundLayer);
+
+        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 5f, 1 << player.gameObject.layer);
 
         float direction = Mathf.Sign(player.position.x - transform.position.x);
 
         if (isGrounded)
         {
-            RaycastHit2D groundInFront = Physics2D.Raycast(transform.position, new Vector2(direction, 0), 2f, groundLayer);
+            RaycastHit2D groundInFront = Physics2D.Raycast(transform.position, new Vector2(direction, 0), 5f, groundLayer);
 
-            RaycastHit2D gapAhead = Physics2D.Raycast(transform.position + new Vector3(direction, 0, 0), Vector2.down, 2f, groundLayer);
+            RaycastHit2D gapAhead = Physics2D.Raycast(transform.position + new Vector3(direction, 0, 0), Vector2.down, 5f, groundLayer);
 
-            RaycastHit2D platformAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, groundLayer);
+            RaycastHit2D platformAbove = Physics2D.Raycast(transform.position, Vector2.up, 5f, groundLayer);
+
+            Debug.DrawRay(transform.position, new Vector2(direction, 1f).normalized * 4f, Color.green);
 
             if (!groundInFront.collider && !gapAhead.collider)
             {
                 shouldJump = true;
+                jumpForceToApply = jumpForce;
             }
             else if (isPlayerAbove && platformAbove.collider)
             {
                 shouldJump = true;
+                jumpForceToApply = jumpForce;
             }
+
+            RaycastHit2D platformAhead = Physics2D.Raycast(transform.position, new Vector2(direction, 1f).normalized, 4f, groundLayer);
+            Debug.DrawRay(transform.position, new Vector2(direction, 1f).normalized * 4f, Color.green);
+
+            if (platformAhead.collider != null)
+            {
+                float distance = Vector2.Distance(transform.position, platformAhead.point);
+                if (distance <= maxReachableDistance)
+                {
+                    float adjustedForce = Mathf.Clamp(distance * 1.2f, 3f, jumpForce);
+                    jumpForceToApply = adjustedForce;
+                    shouldJump = true;
+                }
+            }
+
         }
     }
 
@@ -58,9 +83,11 @@ public class Phantom : MonoBehaviour
         {
             shouldJump = false;
             Vector2 direction = (player.position - transform.position).normalized;
-            Vector2 jumpDirection = direction * jumpForce;
+            Vector2 jumpDirection = direction * jumpForceToApply;
 
             rb.AddForce(new Vector2(jumpDirection.x, jumpForce), ForceMode2D.Impulse);
         }
+
+
     }
 }
